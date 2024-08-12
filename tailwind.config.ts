84d72a9341ae1a467a07647f538970b8b7dff7d8
rask24 <rask0x18@gmail.com>
2024-08-12 19:10:34 +0900

    Add color scheme in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,19 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'base-white': '#FBFFFF',
+        'base-black': '#1F2125',
+        'accent-orange': '#FF8109',
+        'accent-purple': '#948FA8',
+        'accent-dark-purple': '#2C2732',
+        'accent-blue': '#50AFFF',
+        'accent-smoky-blue': '#91AECC',
+        'accent-red': '#D95103',
+        'accent-dark-green': '#354135',
+      },
+    },
   },
   plugins: [],
 };
