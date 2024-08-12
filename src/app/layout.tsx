5f385224c5d1f526fd28ffde0a90893c62606436
Reiya Asuke <70057885+rask24@users.noreply.github.com>
2024-08-12 21:58:54 +0900

    feature/issue#3 (#5)
    * Delete unnecessary code in tailwind.config.ts
    
    * Fix settings.json
    
    * Add color scheme in tailwind.config.ts
    
    * Add custom fonts in tailwind.config.ts
    
    * Add Senobi-Gothic font and Configure in globals.css
    
    * Configure google fonts in layout.tsx
    
    * Fix Google Fonts settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Montserrat } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+});
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja" className={`${inter.variable} ${montserrat.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,24 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'base-white': '#FBFFFF',
+        'base-black': '#1F2125',
+        'accent-orange': '#FF8109',
+        'accent-purple': '#948FA8',
+        'accent-dark-purple': '#2C2732',
+        'accent-blue': '#50AFFF',
+        'accent-smoky-blue': '#91AECC',
+        'accent-red': '#D95103',
+        'accent-dark-green': '#354135',
+      },
+      fontFamily: {
+        main: ['Senobi Gothic', 'sans-serif'],
+        body: ['var(--font-inter)', 'sans-serif'],
+        numeric: ['var(--font-montserrat)', 'sans-serif'],
       },
     },
   },
