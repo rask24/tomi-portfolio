14144d9078789aa28002ead323401704a297095e
rask24 <rask0x18@gmail.com>
2024-08-13 17:16:05 +0900

    Create sample page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
+import PortfolioCard from '@/components/elements/PortfolioCard';
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="font-main">tomi portfolio</h1>
+    <main className="container mx-auto">
+      <h1 className="font-main py-4 text-2xl">tomi portfolio</h1>
+      <div>
+        <PortfolioCard
+          image="/images/sample_icon.png"
+          title="サンプル"
+          description="キャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプション"
+          date={new Date('2024-2-24')}
+        />
+        <PortfolioCard
+          image="/images/sample_icon.png"
+          title="サンプル"
+          description="キャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプション"
+          date={new Date('2024-2-24')}
+        />
+        <PortfolioCard
+          image="/images/sample_icon.png"
+          title="サンプル"
+          description="キャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプション"
+          date={new Date('2024-2-24')}
+        />
+        <PortfolioCard
+          image="/images/sample_icon.png"
+          title="サンプル"
+          description="キャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプションキャプション"
+          date={new Date('2024-2-24')}
+        />
+      </div>
     </main>
   );
 }
