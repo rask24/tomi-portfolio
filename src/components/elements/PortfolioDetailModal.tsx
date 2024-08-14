742795b23819cb47dc7d698a562ecf23d4075a19
rask24 <rask0x18@gmail.com>
2024-08-14 19:39:53 +0900

    Fix PortfolioDetailModal styling

--- src/components/elements/PortfolioDetailModal.tsx
+++ src/components/elements/PortfolioDetailModal.tsx
@@ -23,23 +23,29 @@ function PortfolioDetailModal({ isOpen, onClose, item }: PortfolioDetailModalPro
       <div className="bg-white p-8 rounded-lg max-w-2xl w-full max-h-[90vh] overflow-y-auto relative">
         <button
           onClick={onClose}
-          className="absolute top-4 right-4 text-2xl text-gray-500 hover:text-gray-700"
+          className="absolute top-0 right-4 font-light text-6xl text-accent-orange"
           type="button"
         >
           &times;
         </button>
-        <h2 className="font-main text-4xl text-accent-orange mb-4">{item.title}</h2>
-        <Image src={item.image} width={600} height={400} alt={item.title} className="w-full mb-4" />
-        <p className="font-body text-accent-dark-purple mb-4">{item.description}</p>
+        <Image
+          src={item.image}
+          width={600}
+          height={400}
+          alt={item.title}
+          className="w-full mt-8 mb-4"
+        />
         <p className="font-numeric text-sm text-accent-dark-purple mb-4">{formattedDate}</p>
+        <h2 className="font-main text-4xl text-accent-orange mb-4">{item.title}</h2>
+        <p className="font-body text-accent-dark-purple mb-4">{item.description}</p>
 
         {item.link && (
-          <div className="mb-4">
+          <div className="flex justify-center mb-4">
             <a
               href={item.link.url}
               target={item.link.isExternal ? '_blank' : '_self'}
               rel={item.link.isExternal ? 'noopener noreferrer' : ''}
-              className="block font-main text-base-white bg-accent-orange py-2 px-4 rounded-md w-fit"
+              className="block font-main text-2xl text-base-white bg-accent-orange py-2 px-4 rounded-md w-fit"
             >
               {item.link.text}
             </a>
