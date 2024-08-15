94e70e513d2a93ae24835b139277a1dee77cf22e
rask24 <rask0x18@gmail.com>
2024-08-15 12:49:01 +0900

    Update formatDate

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,7 +1,19 @@
 import { format } from 'date-fns';
 
-function formatDate(date: Date) {
-  return format(date, 'MMM. do yyyy');
+function formatDate(date: Date | string) {
+  try {
+    const dateObj = typeof date === 'string' ? new Date(date) : date;
+
+    if (Number.isNaN(dateObj.getTime())) {
+      throw new Error('Invalid date');
+    }
+
+    return format(dateObj, 'MMM. do yyyy');
+  } catch (error) {
+    console.error('Error formatting date:', error);
+
+    return 'Invalid Date';
+  }
 }
 
 export default formatDate;
