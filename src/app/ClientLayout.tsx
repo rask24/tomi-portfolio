c102f16dc8e1f591a5ea77498f98ea6607560251
rask24 <rask0x18@gmail.com>
2024-09-23 01:44:40 +0900

    Implement MenuGridSidePanel

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -2,14 +2,15 @@
 
 import useHeader from '@/hooks/useHeader';
 import Header from '@/components/layouts/Header';
+import MenuGridSidePanel from '@/components/layouts/MenuGridSidePanel';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const { isMobile } = useHeader();
 
   return (
     <body>
-      {isMobile && <Header />}
-      <main className={isMobile ? 'mt-16' : ''}>{children}</main>
+      {isMobile ? <Header /> : <MenuGridSidePanel />}
+      <main className={isMobile ? 'mt-16' : 'mr-80'}>{children}</main>
     </body>
   );
 }
