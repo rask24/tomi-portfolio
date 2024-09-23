cc90f3e458e24481d3614ed44fc8f7f11be8676e
Reiya Asuke <70057885+rask24@users.noreply.github.com>
2024-09-23 13:53:47 +0900

    Implement MenuGridSidebar (#24)
    * Implement MenuGridSidebar
    
    * Fix eslint

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -1,16 +1,24 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import useHeader from '@/hooks/useHeader';
 import Header from '@/components/layouts/Header';
 import MenuGridSidePanel from '@/components/layouts/MenuGridSidePanel';
+import MenuSidebar from '@/components/layouts/MenuSidebar';
 
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
+type ClientLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function ClientLayout({ children }: ClientLayoutProps) {
   const { isMobile } = useHeader();
+  const pathname = usePathname();
 
   return (
     <body>
-      {isMobile ? <Header /> : <MenuGridSidePanel />}
+      {isMobile && <Header />}
       <main className={isMobile ? 'mt-16' : 'mr-80'}>{children}</main>
+      {!isMobile && (pathname === '/' ? <MenuGridSidePanel /> : <MenuSidebar />)}
     </body>
   );
 }
